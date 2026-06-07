4a99b2d6b637cc4cfd33a1e51cb5623d644600d3
siwy126Pablo <pawel.sikora@controlzet.pl>
2026-06-07 17:14:46 +0200

    feat: add sitemap integration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://aifirmy.pl',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
